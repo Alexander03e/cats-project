308df1d368aa54a4b33213a881873f16934206b0
Namisami <79804458+Namisami@users.noreply.github.com>
2023-12-07 00:32:25 +0300

    add media and login

--- frontend/cats/src/pages/singin/Singin.jsx
+++ frontend/cats/src/pages/singin/Singin.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react"
 import Button from "../../components/Button/Button"
+import axios from 'axios'
 
 const Singin = () => {
+    const [state, setState] = useState({
+        username: '',
+        password: '',
+    })
+
+    const login = async (e) => {
+        await axios
+            .post('http://localhost:8000/api/token/', {
+                username: state.username,
+                password: state.password,
+            })
+            .then(res => window.localStorage.setItem('token', res.data.token))
+            .catch(err => console.log(err))
+    }
 
     return (
         <div className="container">
@@ -9,6 +25,8 @@ const Singin = () => {
             <input
                 type="text"
                 name="username"
+                onChange={e => setState({...state, username: e.target.value})}
+                value={ state.username }
                 placeholder="Логин"
                 required
             />
@@ -16,14 +34,14 @@ const Singin = () => {
             <input
                 type="password"
                 name="password"
-                // value={password}
-                // onChange={handleChange}
+                onChange={e => setState({...state, password: e.target.value})}
+                value={ state.password }
                 placeholder="Пароль"
                 required
             />
             <br />
             
-  <Button>Войти</Button>
+    <Button onClick={ login } >Войти</Button>
 </form>
         </div>
     )
