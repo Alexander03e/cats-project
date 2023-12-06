4ff97b864326da1983096df687bad9899a12d9c9
Namisami <79804458+Namisami@users.noreply.github.com>
2023-12-07 00:33:46 +0300

    fix media

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -28,3 +30,5 @@ urlpatterns = [
     path('api/token/', obtain_auth_token, name='api_token_auth'), 
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
