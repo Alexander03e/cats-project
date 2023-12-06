7936a92a655c5db22fb1be5807a4539b4069baf3
Alexander03e <cfifeg1@gmail.com>
2023-12-07 02:50:03 +0300

    'fix'

--- frontend/cats/src/main.jsx
+++ frontend/cats/src/main.jsx
@@ -12,46 +12,51 @@ const catsURL = 'http://127.0.0.1:8000/api/cats'
 const projectsURL = 'http://127.0.0.1:8000/api/projects'
 const donatsURL = 'http://127.0.0.1:8000/api/donats'
 const getStore = async () => {
-  axios.get(catsURL, {
+  return new Promise(async (resolve, reject) => {
+
+    await axios.get(catsURL, {
+      headers: {
+          Authorization: `Token ${token}`
+      }
+    }).then(res=>{
+    res.data.map(cat => {
+      store.cats.push(cat)            
+    })
+    console.log(store.cats)
+    })
+    await axios.get(projectsURL, {
+      headers: {
+          Authorization: `Token ${token}`
+      }
+    }).then(res=>{
+    res.data.map(project => {
+      store.projects.push(project)
+    })
+    console.log(store.projects)
+    })
+    axios.get(donatsURL, {
     headers: {
-        Authorization: `Token ${token}`
+      Authorization: `Token ${token}`
     }
-  }).then(res=>{
-  res.data.map(cat => {
-    store.cats.push(cat)            
+    }).then(res=>{
+    res.data.map(don => {
+      console.log(don)
+      // store.donats.push(don)
+    })
+    })
+    resolve(store)
   })
-  console.log(store.cats)
-  })
-  axios.get(projectsURL, {
-    headers: {
-        Authorization: `Token ${token}`
-    }
-  }).then(res=>{
-  res.data.map(project => {
-    store.projects.push(project)
-  })
-  console.log(store.projects)
-  })
-  // axios.get(donatsURL, {
-  // headers: {
-  //   Authorization: `Token ${token}`
-  // }
-  // }).then(res=>{
-  // res.data.map(don => {
-  //   console.log(don)
-  //   // store.donats.push(don)
-  // })
-  // })
+  
 }
-getStore()
 function renderContent () {
+  getStore()
   setTimeout(() => {
     ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App store = {store}/>
     </BrowserRouter>
     )
-  }, 1500)
+  }, 1000)
 }
 
 renderContent()
